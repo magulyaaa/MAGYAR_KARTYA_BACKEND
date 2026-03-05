54f9f0da8364a5ca9b324a60be4bf8d84a0d6d1c
diak <diak@mesterlevel.lo>
2026-03-05 12:26:38 +0100

    express.static card_img

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
-const express=require('express')
+const express = require('express')
 const cookieParser = require('cookie-parser')
 const cors=require('cors')
+const path = require('path')
 
 
-const userRoutes=require('./routes/userRoutes')
-const voteRoutes=require('./routes/voteRoutes')
-const uploadRoutes=require('./routes/uploadRoutes')
+const userRoutes = require('./routes/userRoutes')
+const voteRoutes = require('./routes/voteRoutes')
+const uploadRoutes = require('./routes/uploadRoutes')
 const gameRoutes = require("./routes/gameRoutes")
 
 const app=express()
+
+app.use('/card_img', express.static(path.join(__dirname, 'card_img')))
 
 app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-    origin:'htttp://localhost:5173',
+    origin:'http://localhost:5173',
     credentials: true
 }))
 
-app.use('/users/', userRoutes)
+app.use('/users', userRoutes)
 app.use('/api/votes', voteRoutes)
-app.use('/api/',uploadRoutes)
-app.use("/api/game", gameRoutes)
+app.use('/api', uploadRoutes)
+app.use('/api/game', gameRoutes)
 
-
-module.exports=app
+module.exports = app
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const {config}=require('./config/dotenvConfig')
 const app=require('./app')
+const path = require('path')
 
 const HOST=config.HOST
 const PORT=config.PORT
 
 app.listen(PORT, HOST, () => {
+    console.log(path.join(__dirname, 'card_img', 'makk_also.jpg'))
     console.log(`IP: http://${HOST}:${PORT}`)
 })
