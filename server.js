95557b95b0b84b8cff7fc31f4579d0a973d58a23
kpisti18 <k.istvanzoltan@gmail.com>
2026-03-10 08:37:57 +0100

    teszt

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path')
 
 const HOST=config.HOST
 const PORT=config.PORT
+// teszt
 
 app.listen(PORT, HOST, () => {
     console.log(path.join(__dirname, 'card_img', 'makk_also.jpg'))
