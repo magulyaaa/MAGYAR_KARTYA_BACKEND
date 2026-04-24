d51de79b7fe10a8b5da0e877d9e73a0a44cd810e
diak <diak@mesterlevel.lo>
2026-04-24 10:57:02 +0200

    fajerkeszujra

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cors({
 app.use('/users', userRoutes)
 app.use('/api/votes', voteRoutes)
 app.use('/api', uploadRoutes)
-app.use('/api/21', gameRoutes)
+app.use('/api', gameRoutes)
 app.use('/api/fajer', gameRoutes)
 app.use('/api/deleteUser',adminRoutes)
 
